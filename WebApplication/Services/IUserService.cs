1a0ee8561932b893476a02f9ef024c52bcdd6f1c
T9404 <ghubman1@gmail.com>
2023-10-17 21:58:10 +0700

    Created AuthController, UserService
    Added the AuthController class responsible for handling authentication and user registration. Also business logic in userService

---
+++ WebApplication/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication.Entity;
+using WebApplication.Models.Responses;
+
+namespace WebApplication.Services;
+
+public interface IUserService
+{
+    Task<RegistrationResponse> CreateUser(User user);
+}
