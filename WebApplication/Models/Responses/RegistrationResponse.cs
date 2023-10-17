a95ce3d185574e3200e46f8aabb611a1c8bf5e26
T9404 <ghubman1@gmail.com>
2023-10-17 22:01:20 +0700

    Added gender, registration response, user entity
    In this commit, I introduced the 'Gender' enumeration, allowing us to represent gender information. Additionally, I created two essential models: 'RegistrationResponse' to handle registration responses, and 'User' entity to represent user data.

---
+++ WebApplication/Models/Responses/RegistrationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication.Models.Responses;
+
+public record RegistrationResponse(string FullName, string Email)
+{
+    public RegistrationResponse() : this(string.Empty, string.Empty)
+    {
+    }
+}
