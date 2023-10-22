e2ed24c2e02811f54a0b5c09081f01f3a58268e4
T9404 <ghubman1@gmail.com>
2023-10-22 20:18:52 +0700

    Added order entity with requirement fields
    Implement order class in project

---
+++ WebApplication/Entity/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication.Entity;
+
+[Table("orders")]
+public class Order
+{
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+    
+    [Column("delivery_time")]
+    public DateTime DeliveryTime { get; set; }
+    
+    [Column("order_time")]
+    public DateTime OrderTime { get; set; }
+    
+    [Column("status")]
+    public string Status { get; set; } = null!;
+    
+    [Column("price")]
+    public int Price { get; set; }
+    
+    [Column("dishes")]
+    public List<Guid> Dishes { get; set; } = null!;
+    
+    [Column("address")]
+    public string Address { get; set; } = null!;
+}
